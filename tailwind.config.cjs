7326d392e4e9f2331eb324e18a709a61bc0c2442
Daniel Ruben Rivas Saltos <rubenrivas_17@hotmail.com>
2026-07-07 21:19:49 -0500

    CM 1: Estrucutura basica de la paginna

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#CA2C92'
+      }
+    }
+  },
+  plugins: []
+};
